36221ab3ddb4c772d21574c1f0072926ed311839
muhsinabdullah <muhsinabdullah1250@gmail.com>
2025-12-30 22:08:04 +0600

    Project Structure ready

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <title>Home</title>
+            Home
+        </div>
+    );
+};
+
+export default Home;
